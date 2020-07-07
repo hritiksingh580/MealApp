87d3826461eeae97c2699f47e87cbc9a2a15872f
Narayan Singh Solnalki <hritiksingh580@gmail.com>
2020-07-07 20:21:26 +0530

    Creating the Form For Editing Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +11,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editable = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params
@@ -18,9 +21,33 @@ export class RecipeEditComponent implements OnInit {
         (parms: Params) => {
           this.id = parms['id'],
           this.editable = parms['id'] != null;
-          console.log(this.editable)
+          this.initForm();
         }
       )
+  } 
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let reccipeDescription = "";
+
+    if(this.editable) {
+    const recipes = this.recipeService.getRecipeWithIndex(this.id);
+
+      recipeName = recipes.name;
+      recipeImagePath = recipes.imagePath;
+      reccipeDescription = recipes.name
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(reccipeDescription),
+    })
   }
 
 }
