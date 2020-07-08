ff354d8a69435d506c177b079c1830256031af08
Narayan Singh Solnalki <hritiksingh580@gmail.com>
2020-07-08 23:14:53 +0530

    Deleting Ingredients and Some Finishing Touches

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -87,4 +87,8 @@ export class RecipeEditComponent implements OnInit {
     )
   }
 
+  onDeleteIngredients(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Recipes } from '../recipes.model'
 import { RecipeService } from '../recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy{
+  subscription: Subscription;
 
   recipes: Recipes[] = []
 
@@ -17,11 +19,17 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipeChange.subscribe(
+    this.subscription = this.recipeService.recipeChange.subscribe(
       (recipe: Recipes[]) => {
         this.recipes = recipe;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
   }
 
 }
